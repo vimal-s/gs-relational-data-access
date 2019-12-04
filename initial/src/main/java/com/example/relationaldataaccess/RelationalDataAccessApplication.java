ccb2a5177a5e3be8d776907b801c2bcdef63792d
vimal <vimalsingh99.2@gmail.com>
2019-12-04 20:33:24 +0530

    Reformat code

--- initial/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
+++ initial/src/main/java/com/example/relationaldataaccess/RelationalDataAccessApplication.java
@@ -43,7 +43,10 @@ public class RelationalDataAccessApplication implements CommandLineRunner {
         log.info("Querying for customer records where first name = 'Josh':");
         jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
                 new Object[]{"Josh"},
-                ((rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))))
+                ((rs, rowNum) ->
+                        new Customer(rs.getLong("id"),
+                                rs.getString("first_name"),
+                                rs.getString("last_name"))))
                 .forEach(customer -> log.info(customer.toString()));
     }
 }
